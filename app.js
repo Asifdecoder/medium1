75c1412070a6ce880b93daf529c77381fabb4795
Asif <khanasif1654@gmail.com>
2023-12-15 11:58:11 +0530

    updated js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+let nav = document.querySelector(".nav");
+let getbutton = document.getElementById("getStarted");
+console.log(scrollY)
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 393.6) {
+    nav.style.backgroundColor = "white";
+    getbutton.style.backgroundColor = "rgb(26, 137, 23)";
+  } else {
+    nav.style.backgroundColor = "#ffc017";
+    getbutton.style.backgroundColor = "black";
+  }
+});
 
